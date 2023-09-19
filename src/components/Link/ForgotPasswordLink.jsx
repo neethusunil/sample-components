03047b81f3a62ce4d60f65bd5a3ac11f756b4eb3
neethusunil <neethusunil488@gmail.com>
2023-09-19 15:35:39 +0900

    Added: Link component

---
+++ src/components/Link/ForgotPasswordLink.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Link.css'
+
+const ForgotPasswordLink = () => {
+  return (
+    <div className="forgot-password">
+      <a href="/forgot-password">Forgot Password?</a>
+    </div>
+  );
+};
+
+export default ForgotPasswordLink;
