8bc9eb77bc20dc318da708e7bccbb0763fb0c1ad
neethusunil <neethusunil488@gmail.com>
2023-09-20 15:32:53 +0900

    Added: page with table

--- src/App.js
+++ src/App.js
@@ -6,11 +6,87 @@ import Textfield from './components/Fields/Textfield';
 import './components/Fields/Fields.css'
 import './components/formStyle.css'
 import overlap from './components/overlap.png'
+import ForgotPasswordLink from './components/Link/ForgotPasswordLink';
+import Navbar from './components/Navbar/Navbar';
+import Tableres from './components/table/Table';
+import Table from './components/table/Table';
+import '../src/components/tablePageStyle.css'
+import CompanyName from './components/Headings/CompanyName';
+import TableHeading from './components/Headings/TableHeading';
 
 function App() {
+  const theadData = ["Name", "Poles", "Podiums", "Wins", "Career Points", "Championships"];
+
+  const bodyData = [
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    {
+      Name: "Max Verstappen",
+      Poles: 22,
+      Podiums: 80,
+      Wins: 37,
+      "Career Points": 2080.5,
+      Championships: 2,
+    },
+    // Add more data objects as needed
+  ];
+
   return (
     <>
-    <div className="parent"> 
+    <Navbar/>
+    <div className='company-and-btn'>
+      <CompanyName text="Farmer"/>
+      <Button className= 'create-btn' text="Create New Company"/>
+    </div>
+    <TableHeading text="User List"/>
+    <Table theadData={theadData} bodyData={bodyData} />
+    {/* <div className="parent"> 
       <div className="centered-container">
         <div className="form-container">
           <h1>Login</h1>
@@ -25,11 +101,12 @@ function App() {
               // onChange={handlePasswordChange}
             />
             <Button text="Login" className="form-btn" />
+            <ForgotPasswordLink />
             
           </form>
         </div>
       </div>
-    </div>
+    </div> */}
     <img src={overlap} alt="Stacked Image" class="stacked-image" />
     </>
     
